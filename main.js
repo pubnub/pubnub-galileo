5111c357d85eff43be1000dc2d1961e970dadde3
Ian Jennings <ian@meetjennings.com>
2015-04-09 21:42:07 -0500

    merge real demo

--- main.js
+++ main.js
@@ -1,2 +1,36 @@
-var console = console || false;
-console.log('we got it running');
+var mraa = require('mraa');
+var PUBNUB = require('pubnub');
+
+console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
+
+var analogPin0 = new mraa.Aio(0); //setup access analog input Analog pin #0 (A0)
+
+var pubnub = PUBNUB.init({
+  publish_key: 'demo',
+  subscribe_key: 'demo'
+});
+
+var lastValue = 0;
+setInterval(function(){
+    var analogValue = analogPin0.read(); //read the value of the analog pin
+
+    var max = 1024;
+    var percent = ((analogValue / max) * 100).toFixed(2);
+
+    console.log(percent);
+
+    if(percent !== lastValue) {
+
+        pubnub.publish({
+            channel: 'pubnub-intel-gal-demo',
+            message: {
+              columns: [
+                ['Potentiometer', percent]
+              ]
+            }
+        });
+
+        lastValue = percent;
+    }
+
+}, 500);
